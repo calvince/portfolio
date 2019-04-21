1c8e379fc7f5efd881253bad6e969ebe450deae7
Omonge Calvince <calvo-linus@localhost.localdomain>
2019-04-21 09:19:08 +0300

    using script language to enable typed.js

---
+++ javascript/enyewe.js
@@ -0,0 +1,12 @@
+
+
+var omonge = new Typed(".yes", {
+    strings: ["Software Developer",
+     "Manchester Fan",
+     "Student @ Moringa School",
+     "Rugby Player"],
+    typeSpeed:60,
+    backSpeed:60,
+    loop:true
+
+  });
